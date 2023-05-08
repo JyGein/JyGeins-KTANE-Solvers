742048b6b22b7fff7e65bb8b9b5aa4ddf72255b1
JyGein <52318346+JyGein@users.noreply.github.com>
2023-05-08 06:30:36 -0400

    Making it local folders (#3)
    r'C:\Users\jygei\Desktop\ktane autosolvers\code\modules' to r'.\modules'

--- JyGeins KTANE solvers.py
+++ JyGeins KTANE solvers.py
@@ -2,7 +2,7 @@ import importlib
 import sys
 import os
 
-sys.path.insert(0, r'C:\Users\jygei\Desktop\ktane autosolvers\code\modules')
+sys.path.insert(0, r'.\modules')
 module = input('What module?: ')
 modulename = importlib.import_module(module)
 modulename.run()
