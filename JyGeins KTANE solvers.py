dcfdd618766c60152c1c626440fb29515dbeaf03
Taser <81585596+TaserTheFox@users.noreply.github.com>
2023-05-10 20:54:17 -0700

    Update JyGeins KTANE solvers.py
    now you dont have to run the exe every time! and to end it, oyu just close the console!

--- JyGeins KTANE solvers.py
+++ JyGeins KTANE solvers.py
@@ -3,8 +3,7 @@ import sys
 import os
 
 sys.path.insert(0, r'.\modules')
-module = input('What module?: ')
-modulename = importlib.import_module(module)
-modulename.run()
-
-os.system("pause")
+while True:
+  module = input('What module?: ')
+  modulename = importlib.import_module(module)
+  modulename.run()
